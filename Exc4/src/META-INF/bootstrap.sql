6bbc1d72d53d610bdc11bdd3ab4ef5d3b1e4ebe9
Johan Mickos <jmickos@bu.edu>
2016-12-07 12:14:01 +0100

    Complete SQL import data

--- Exc4/src/META-INF/bootstrap.sql
+++ Exc4/src/META-INF/bootstrap.sql
@@ -4,11 +4,25 @@ INSERT INTO conversion VALUES ( 3, 1, 'ISK', 'ISK');
 INSERT INTO conversion VALUES ( 4, 1, 'EUR', 'EUR');
 
 
-INSERT INTO conversion VALUES ( 5, 1, 'SEK', 'USD');
-INSERT INTO conversion VALUES ( 6, 1, 'USD', 'SEK');
+INSERT INTO conversion VALUES ( 5, 0.11, 'SEK', 'USD');
+INSERT INTO conversion VALUES ( 6, 9.12, 'USD', 'SEK');
 
-INSERT INTO conversion VALUES ( 7, 1, 'SEK', 'ISK');
-INSERT INTO conversion VALUES ( 8, 1, 'ISK', 'SEK');
+INSERT INTO conversion VALUES ( 7, 12.12, 'SEK', 'ISK');
+INSERT INTO conversion VALUES ( 8, 0.082, 'ISK', 'SEK');
 
-INSERT INTO conversion VALUES ( 9, 1, 'SEK', 'EUR');
-INSERT INTO conversion VALUES (10, 1, 'EUR', 'SEK');
+INSERT INTO conversion VALUES ( 9, 0.1, 'SEK', 'EUR');
+INSERT INTO conversion VALUES (10, 10, 'EUR', 'SEK');
+
+INSERT INTO conversion VALUES (11, 0.93, 'USD', 'EUR');
+INSERT INTO conversion VALUES (12, 1.07, 'EUR', 'USD');
+
+INSERT INTO conversion VALUES (13, 110.55, 'USD', 'ISK');
+INSERT INTO conversion VALUES (14, 0.0090, 'ISK', 'USD');
+
+INSERT INTO conversion VALUES (15, 0.0084, 'ISK', 'EUR');
+INSERT INTO conversion VALUES (16, 118.60, 'EUR', 'ISK');
+
+
+
+
+
